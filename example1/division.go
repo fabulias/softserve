89d4c4bac9598b12dd61d013f1cfb35882d79902
fabulias <farid.abulias@gmail.com>
2024-12-09 18:54:48 -0300

    feat: add division by zero function

--- example1/division.go
+++ example1/division.go
@@ -3,3 +3,10 @@ package example1
 func Division(a, b int) int {
 	return a / b
 }
+
+//func DivisionWithError(a, b int) (int, error) {
+//	if b == 0 {
+//		return 0, fmt.Errorf("b value is zero")
+//	}
+//	return a / b, nil
+//}
--- example1/division_test.go
+++ example1/division_test.go
@@ -5,21 +5,26 @@ import (
 )
 
 type DivisionTestDefinition struct {
-	name     string
-	a, b     int
-	expected int
+	name       string
+	a, b       int
+	expected   int
+	shouldFail bool
 }
 
 var divisionTest []DivisionTestDefinition = []DivisionTestDefinition{
-	{"1/1", 1, 1, 1},
-	{"2/3", 2, 3, 0},
-	{"6/2", 6, 2, 3},
+	{"1/1", 1, 1, 1, true},
+	{"2/3", 2, 3, 0, true},
+	{"6/2", 6, 2, 3, true},
 }
 
 func TestAdd_GoodExample(t *testing.T) {
 	for _, tt := range divisionTest {
 		t.Run(tt.name, func(t *testing.T) {
 			result := Division(tt.a, tt.b)
+			//result, err := DivisionWithError(tt.a, tt.b)
+			//if (err != nil) != tt.shouldFail {
+			//	t.Error("unexpected error:", err)
+			//}
 			if result != tt.expected {
 				t.Errorf("got %d, want %d", result, tt.expected)
 			}
@@ -30,5 +35,11 @@ func TestAdd_GoodExample(t *testing.T) {
 func TestAdd_BadExample(t *testing.T) {
 	if result := Division(1, 1); result != 1 {
 		t.Errorf("Division(1,1); got %d, want %d", result, 1)
+	}
+	if result := Division(2, 3); result != 0 {
+		t.Errorf("Division(2,3); got %d, want %d", result, 1)
+	}
+	if result := Division(6, 2); result != 3 {
+		t.Errorf("Division(6,2); got %d, want %d", result, 1)
 	}
 }
